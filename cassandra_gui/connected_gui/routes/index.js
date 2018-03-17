0378670de812fdda5d911d29e0d5df071c202a37
Austin Jiang <austinjiang@Austins-MacBook-Pro.local>
2018-03-17 00:52:45 -0400

    cass

--- cassandra_gui/connected_gui/routes/index.js
+++ cassandra_gui/connected_gui/routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var cassandra = require('cassandra-driver');
+
+var client = new cassandra.Client({contactPoints: ['127.0.0.1']});
+
+client.connect(function(err, result({
+  console.log('index: cassandra connected');
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
